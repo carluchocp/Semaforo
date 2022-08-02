cb0ada510069e60e08049f3676947773794a3c64
Carlo Carpentieri <carluchocp1406@gmail.com>
2022-08-02 10:12:51 -0400

    ready to deploy

---
+++ src/js/component/Light.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Light = (props)=>{
+    return(
+        <div className={`light ${props.color} ${props.activeLight === props.color ? "active" : ""}`} onClick={()=> props.handleLight(props.color)}></div>
+    );
+}
+export default Light
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,20 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
+import Light from "./Light.jsx";
 
 //create your first component
 const Home = () => {
+	const [activeLight, setActiveLight] = useState("")
+	const handleLight = (color) => {
+		setActiveLight(color)
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container d-flex flex-column align-items-center">
+			<div className="palito"></div>
+			<div className="semaforo d-flex flex-column align-items-center justify-content-around rounded py-1">
+			<Light color={"red"} activeLight={activeLight} handleLight={handleLight}/>
+			<Light color={"yellow"} activeLight={activeLight} handleLight={handleLight}/>
+			<Light color={"green"} activeLight={activeLight} handleLight={handleLight}/>
+			</div>
 		</div>
 	);
 };
